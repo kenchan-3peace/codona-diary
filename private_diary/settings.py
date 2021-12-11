3a45e7cd7fd952ba2921bc43fe48bc2d998e153c
codonastyle <kenchan.3peace@gmail.com>
2021-12-11 14:35:44 +0900

    no message

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .settings_common import *
 
 ALLOWED_HOSTS = ['codona-diary.herokuapp.com']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
