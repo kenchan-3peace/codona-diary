6bc68e76a25b916126c1ec38d372f6ccb69db059
codonastyle <kenchan.3peace@gmail.com>
2021-12-11 11:50:47 +0900

    Revert "heroku用に編集"
    This reverts commit cffa357292860794d021e5cb319d6c21f3ad3ef4.

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,5 +176,3 @@ LOGGING = {
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-# Activate Django-Heroku.
-django_heroku.settings(locals())
